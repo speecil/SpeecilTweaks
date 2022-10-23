2658edafb55609a7c3d53c89862a9258e9fd17c3
speecil <90689870+speecil@users.noreply.github.com>
2022-10-23 20:57:56 +1100

    optimisation

--- src/main.cpp
+++ src/main.cpp
@@ -46,13 +46,6 @@ DEFINE_CONFIG(MainConfig);
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be removed if those are in use
-Configuration & getConfig() {
-  static Configuration config(modInfo);
-  return config;
-}
-
 // Returns a logger, useful for printing debug messages
 Logger & getLogger() {
   static Logger * logger = new Logger(modInfo);
@@ -207,7 +200,6 @@ void setup(ModInfo & info) {
   info.version = VERSION;
   modInfo = info;
 
-  getConfig().Load();
   getLogger().info("Completed setup!");
 }
 
